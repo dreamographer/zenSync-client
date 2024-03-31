fcb4ca307ca379302d0e9bd8df44ff790a3930ee
Ashwin <ashwinkv.akv@gmail.com>
2024-03-31 23:01:43 +0530

    Fix Bug cookie issue

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,34 +1,34 @@
+"use client"
 import dynamic from "next/dynamic";
 import React from "react";
-import { getServerSideUser } from "@/lib/token-utils";
-import { cookies } from "next/headers";
+import useSWR from "swr";
 import Link from "next/link";
-import { redirect } from "next/navigation";
-const MainLayout = async ({ children }: { children: React.ReactNode }) => {
+import axios from "axios";
+const fetcher = (url:string) =>
+axios.get(url, { withCredentials: true }).then(res => res.data);
+const MainLayout = ({ children }: { children: React.ReactNode }) => {
+  const url= `${process.env.NEXT_PUBLIC_SERVER_URL}/auth/users/me`
   const VideoRoomToggle = dynamic(
     () => import("@/components/global/VideoRoomToggle"),
     {
       ssr: false,
     }
   );
-  const nextCookies = cookies();
-  const token = nextCookies.get("jwt")?.value;
-  const refreshToken = nextCookies.get("refreshToken")?.value;
-  console.log("Token and refresh Tokern", token, refreshToken);
-  const user = await getServerSideUser();
-  if (token && refreshToken) {
-    if (user) {
-      return(<>
-        <VideoRoomToggle />
-        {children}
-      </>);
-    } else {
-      redirect("/");
-    }
+   const { data: user, error } = useSWR(url, fetcher);
+
+  console.log(user);
+  
+  if (user) {
+      return (
+        <>
+          <VideoRoomToggle />
+          {children}
+        </>
+      );
   } else {
     return (
       <main>
-        LOGIN TO CONTINUE <Link href={"/login"}>LOGIN</Link>{" "}
+        LOGIN TO CONTINUE <Link href={"/login"}>LOGIN Now</Link>{" "}
       </main>
     );
   }
